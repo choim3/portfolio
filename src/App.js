df08fae935494638735598776cec519938266260
matt <choim3@spu.edu>
2021-05-13 16:09:31 -0700

    added blog route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "./components/home";
 import Projects from "./components/projects";
+import Blogs from "./components/blogs";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/portfolio" component={Projects} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/blogs" component={Blogs} />
         </Switch>
       </div>
     </Router>
