c8639b65cf15a2d8e0875ad444730cde6e293903
matt <choim3@spu.edu>
2021-05-23 20:43:48 -0700

    header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./components/home";
 import Projects from "./components/projects";
 import Blogs from "./components/blogs";
 import Hooks from "./components/hooks";
+import Header from "./components/header";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/projects" component={Projects} />
---
+++ src/components/header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
